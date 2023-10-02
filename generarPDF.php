7f23cdfc384d5518a4305ba7dc4be0e450363d29
Ego <augurto.17@gmail.com>
2023-10-02 18:19:53 -0500

    nuevo pdf

--- generarPDF.php
+++ generarPDF.php
@@ -10,7 +10,7 @@ $pdf->SetFont('Arial', '', 12);
 
 // Obtener la fecha actual en la zona horaria de Perú
 date_default_timezone_set('America/Lima');
-$fecha = date('d/m/Y');
+$fecha = strftime('%d de %B de %Y');
 
 // Agregar la fecha
 $pdf->Cell(0, 10, 'Fecha: ' . $fecha, 0, 1, 'R');
@@ -25,10 +25,19 @@ $pdf->Ln(50); // 5 saltos de línea
 $nombre = $_POST['nombres']; // Asegúrate de obtener el valor del formulario adecuadamente
 $pdf->Cell(0, 10, 'SR: ' . $nombre, 0, 1, 'L');
 
-// Agregar el texto del cuerpo de la carta
-$textoCuerpo = "PRESIDENTE DE LA JUNTA PARROQUIAL\nPRESENTE\n\nApreciable Sr. Trinquete Chanchullo, por medio de este oficio se le hace comunicación de la resolución de la junta directiva de esta empresa, en relación a su comportamiento y manejo de los recursos económicos de la sucursal bajo su cargo. Según los reportes anteriores que hemos tenido, aunadas a las quejas de malos tratos recibidos por usted por parte de sus subordinados, comunicándole el dictamen de la junta directiva, consistente en la resolución de separarlo del cargo que ostenta dentro de esta empresa y pedirle su renuncia. Adjuntando documentos que prueban diversos malos manejos, así como se adjuntan testimonios respectivos a los malos tratos. Atentamente";
+// Texto del cuerpo de la carta
+$textoCuerpo = "Requiero: " . $_POST['Requerimiento'] . "\n\n";
+$textoCuerpo .= "Adjuntando documentos que prueban diversos malos manejos, así como se adjuntan testimonios respectivos a los malos tratos.\n";
+$textoCuerpo .= "Atentamente";
 
+// Agregar el texto del cuerpo de la carta
 $pdf->MultiCell(0, 10, $textoCuerpo, 0, 'J');
+
+// Agregar la imagen de la firma
+$pdf->Image('assets/images/firma.png', 10, $pdf->GetY() + 10, 50);
+
+// Agregar el texto "Firma" debajo de la imagen
+$pdf->Cell(0, 10, 'Firma', 0, 1, 'C');
 
 // Salida del PDF
 $pdf->Output();
