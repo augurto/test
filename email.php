81810698c90bd1cb9ab39ae41988456a4dee9d27
Ego <augurto.17@gmail.com>
2023-10-09 19:10:40 -0500

    stm

--- email.php
+++ email.php
@@ -3,7 +3,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 require 'PHPMailer/src/Exception.php';
+
 
 $mail = new PHPMailer(true);
 
