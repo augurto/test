90e24870e6e7dc0c9544fcf382fb30bef2a07557
Ego <augurto.17@gmail.com>
2023-10-10 12:34:33 -0500

    email

--- email.php
+++ email.php
@@ -6,6 +6,36 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 require 'PHPMailer/src/Exception.php';
 
+// Configuración de la conexión a la base de datos
+$servername = "localhost"; // Cambia esto si tu servidor de base de datos no está en localhost
+$username = "u291982824_test";
+$password = "21.12*Test";
+$database = "u291982824_test";
+
+// Crear la conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Consulta SQL para obtener correos de la tabla "usuarios"
+$sql = "SELECT email FROM usuarios";
+$result = $conn->query($sql);
+
+// Array para almacenar los correos
+$correos = array();
+
+if ($result->num_rows > 0) {
+    // Recorre los resultados y agrega los correos al array
+    while ($row = $result->fetch_assoc()) {
+        $correos[] = $row["email"];
+    }
+}
+
+// Cerrar la conexión a la base de datos
+$conn->close();
 
 $mail = new PHPMailer(true);
 
@@ -32,6 +62,10 @@ try {
 
     $mail->send();
     echo 'El correo se ha enviado correctamente.';
+    
+    // Redireccionar a index.php después de enviar el correo
+    header("Location: index.php");
+    exit();
 } catch (Exception $e) {
     echo 'Error al enviar el correo: ' . $mail->ErrorInfo;
 }
