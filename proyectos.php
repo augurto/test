aac58eb1b059fa622c573c50d7c241f5bcf38682
Ego <augurto.17@gmail.com>
2023-11-07 18:00:15 -0500

    id de proyecto

--- proyectos.php
+++ proyectos.php
@@ -158,15 +158,13 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
 
                                         while ($row = $result->fetch_assoc()) {
                                             echo '<tr>';
-                                            echo '<td>' . $row['id_proyecto'] . '</td>';
-                                            echo '<td>' . $row['nombre'] . '</td>';
+                                            echo '<td><a href="proyecto.php?id=' . $row['id_proyecto'] . '">' . $row['nombre'] . '</a></td>';
                                             echo '<td>' . $row['descripcion'] . '</td>';
                                             echo '<td>' . $row['fecha_inicio'] . '</td>';
                                             echo '<td>' . $row['fecha_estimada_fin'] . '</td>';
                                             echo '<td>' . $row['fecha_real_fin'] . '</td>';
                                             echo '</tr>';
                                         }
-
                                         echo '</tbody>';
                                         echo '</table>';
                                     } else {
