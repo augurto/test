1da26957372b8ca3ae8329e087273871558c7a4e
Ego <augurto.17@gmail.com>
2023-11-20 11:33:30 -0500

    XD

--- includes/guardar_datos_usuario.php
+++ includes/guardar_datos_usuario.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         // Redireccionar según el tipo de usuario
         if ($tipoUsuario == 1) {
-            header("Location: ../vendedor.php");
+            header("Location: ../index2.php");
             exit();
         } elseif ($tipoUsuario == 2) {
             header("Location: ../inicio.php");
